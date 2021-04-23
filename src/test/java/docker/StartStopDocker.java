720841af649398655a59003b3b32f830be8281d4
apreeti-tw <apreeti@thoughtworks.com>
2021-04-23 11:52:11 +0530

    Remove output.txt file deletion

--- src/test/java/docker/StartStopDocker.java
+++ src/test/java/docker/StartStopDocker.java
@@ -4,15 +4,12 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import resources.DockerOperation;
 
-import java.io.File;
 import java.io.IOException;
 
 public class StartStopDocker {
     @BeforeSuite
     public void startDocker() throws IOException, InterruptedException {
-        File file = new File("output.txt");
-        if (file.delete())
-            DockerOperation.docker("start", "Started Selenium Hub");
+        DockerOperation.docker("start", "Started Selenium Hub");
     }
 
     @AfterSuite
