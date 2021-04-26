042e43a50de114a656941c64acac0080894d67a9
apreeti-tw <apreeti@thoughtworks.com>
2021-04-26 15:00:20 +0530

    Add Cart tests to testng.xml

--- src/main/java/resources/Utilities.java
+++ src/main/java/resources/Utilities.java
@@ -10,6 +10,7 @@ import java.util.Random;
 public class Utilities extends Base{
     private static By successAlert = By.cssSelector(".alert-success");
     private static By dangerAlert = By.cssSelector(".alert-danger");
+    private static By modalClose = By.xpath("//button[@class='close']/span");
 
     public static WebElement getSuccessAlert(){
         return driver.findElement(successAlert);
@@ -24,7 +25,7 @@ public class Utilities extends Base{
     }
 
     public static void closeModal() {
-        driver.findElement(By.xpath("//button[@class='close']/span")).click();
+        driver.findElement(modalClose).click();
     }
 
     public static void explicitlyWaitForClickable(WebElement webElement){
